a5d05711dd44ec65b3bc08f3c4b2bd3d855921ab
Michele Zedda <151569776+michelezedda@users.noreply.github.com>
2023-12-04 19:40:47 +0100

    Add files via upload

---
+++ assets/scripts/script.js
@@ -0,0 +1,39 @@
+const mainWrapper = document.getElementById('main-container');
+const counterWrapper = document.getElementById('counter-container');
+const buttonWrapper = document.getElementById('buttons-container');
+
+// COUNTER
+let count = 0;
+const counter = document.createElement('p');
+counter.textContent = '0';
+counterWrapper.appendChild(counter);
+// DECREASE-BUTTON
+const decrease = document.createElement('button');
+decrease.setAttribute('class', 'decrease-button'); 
+decrease.textContent = '-';
+buttonWrapper.appendChild(decrease);
+// RESET-BUTTON
+const reset = document.createElement('button');
+reset.setAttribute('class', "reset-button");
+reset.textContent = 'Reset';
+buttonWrapper.appendChild(reset);
+// INCREASE-BUTTON
+const increase = document.createElement('button');
+increase.setAttribute('class', 'increase-button'); 
+increase.textContent = '+';
+buttonWrapper.appendChild(increase);
+// DECREASE FUNCTION
+mainWrapper.appendChild(decrease).onclick = function() {
+  count -= 1;
+  counterWrapper.appendChild(counter).innerText = count;
+};
+// RESET FUNCTION
+mainWrapper.appendChild(reset).onclick = function() {
+  count = 0;
+  counterWrapper.appendChild(counter).innerText = count;
+};
+// INCREASE FUNCTION
+mainWrapper.appendChild(increase).onclick = function() {
+  count += 1;
+  counterWrapper.appendChild(counter).innerText = count;
+};
